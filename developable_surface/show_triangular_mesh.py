d562b34abefc0fef84cc55c9826bbefde2b3f471
jacky09299 <jacky09299@gmail.com>
2024-02-04 19:25:26 +0800

    Update show_triangular_mesh.py

--- developable_surface/show_triangular_mesh.py
+++ developable_surface/show_triangular_mesh.py
@@ -59,7 +59,7 @@ class Mesh:
             ax.set_zlabel('Z')
 
             # Set the initial view
-            ax.view_init(elev=90, azim=0)  # Adjust these values as needed
+            ax.view_init(elev=80, azim=0)  # Adjust these values as needed
             plt.show()
 
 
